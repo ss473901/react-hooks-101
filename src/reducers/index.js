d7e50295ab0743f9afb98bd6a409e2c6caa35988
Satoshi Sanada <satoshi.473901.1205@gmail.com>
2022-02-04 04:07:37 +0900

    implement event creation

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,10 +5,13 @@ const events = (state = [], action) => {
       const length = state.length;
       const id = length === 0 ? 1 : state[length - 1].id + 1;
       return [...state, { id, ...event }];
+
     case "DELETE_EVENT":
       return state;
+
     case "DELETE_ALL_EVENTS":
       return [];
+
     default:
       return state;
   }
