245181a4bafce6cb56b9c837dd2004ab731c7f3a
Satoshi Sanada <satoshi.473901.1205@gmail.com>
2022-02-03 22:30:32 +0900

    implement callbaks with useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const App = (props) => {
   const [state, setState] = useState(props);
   const { name, price } = state;
 
+  useEffect(() => {
+    console.log("This is like componentDiMount or componentDidUpdate.");
+  });
+
+  useEffect(() => {
+    console.log("This is like componentDiMount");
+  }, []);
+
+  useEffect(() => {
+    console.log("This callback is for name only.");
+  }, [name]);
+
   return (
     <>
       <p>
-        現在の{state.name}は、{state.price}円です。
+        現在の{state.name}は、{price}円です。
       </p>
       <button onClick={() => setState({ ...state, price: price + 1 })}>
         +1
